486dae7eea1181c0d6cbca0fd552585b7eb7a09e
elezibeth <elezibethmorgan@my.unt.edu>
2021-04-01 16:39:04 -0500

    Initial commit.

---
+++ MovieLibraryAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MovieLibraryAPI.Data;
+using MovieLibraryAPI.Models;
+
+namespace MovieLibraryAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+            builder.Entity<Movie>()
+             .HasData(
+                new Movie { Id = 1, Title = "The Departed", Genre = "Drama", DirectorName = "Martin Scorsese" },
+                new Movie { Id = 2, Title = "The Dark Knight", Genre = "Drama", DirectorName = "Christopher Nolan" },
+                new Movie { Id = 3, Title = "Inception", Genre = "Drama", DirectorName = "Christopher Nolan" }
+                );
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+    }
+}
---
+++ MovieLibraryAPI/Migrations/20210401211529_initial.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieLibraryAPI.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DirectorName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "DirectorName", "Genre", "Title" },
+                values: new object[] { 1, "Martin Scorsese", "Drama", "The Departed" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "DirectorName", "Genre", "Title" },
+                values: new object[] { 2, "Christopher Nolan", "Drama", "The Dark Knight" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "DirectorName", "Genre", "Title" },
+                values: new object[] { 3, "Christopher Nolan", "Drama", "Inception" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
